c04fce3f29032bb026ce4f653d3a10016632acd9
evolotek-pratik <pratik.dhane@evolotek.ai>
2026-09-06 14:58:06 +0530

    jenkins testing

--- src/test/java/com/example/demo/controller/HelloControllerTest.java
+++ src/test/java/com/example/demo/controller/HelloControllerTest.java
@@ -19,6 +19,6 @@ class HelloControllerTest {
     void shouldReturnHelloJenkins() throws Exception {
         mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello Jenkins!"));
+                .andExpect(content().string("Hello Pratik!"));
     }
 }
